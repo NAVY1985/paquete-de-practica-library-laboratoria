4532a821b3ad5af65fe586db6e99e97bf219fec4
navy <jezreelsanz@gmail.com>
2018-07-19 16:20:46 -0500

    se suben archivos

---
+++ src/alanPoe.js
@@ -0,0 +1,27 @@
+(function() {
+
+
+
+
+
+    const alanPoe = (number) => {
+
+        let crow = "";
+        for (let i = 0; i < number; i++) {
+            crow += 'nunca mas';
+        }
+        return crow;
+
+    };
+
+    if (typeof window == 'undefined') {
+        console.log('consola');
+        module.exports = alanPoe;
+
+    } else {
+        console.log('navegador');
+        window.alanPoe = alanPoe;
+
+    }
+
+})();
